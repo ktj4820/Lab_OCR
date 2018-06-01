06ec6fd22228346583126624fcd34ff880854b5e
dby_freedom <duboyabz@gmail.com>
2018-06-01 16:59:36 +0800

    add the test module for test function module

--- Image_preprocessing/image_gray.py
+++ Image_preprocessing/image_gray.py
@@ -7,6 +7,7 @@ import cv2
 def gen_thresh_img(cut_image, threshold, mode='binary'):
     """
     依据图像亮度，进行图像二值化（保留原图像）
+    注：为区分红色与灰色，进行列切分时候将不再采用RGB进行切分，而是转换到LAB空间
     :param cut_image:
     :param threshold:
     :param mode:
@@ -116,10 +117,10 @@ def row_split(raw_img_input):
                 break
         return end_
 
-    arg = False
+    arg = True
     '''arg = True表示黑底白字, arg = False表示白底黑字'''
-    if row_black_num_max > row_white_num_max:
-        arg = True
+    if row_black_num_max < row_white_num_max:
+        arg = False
 
     row_mark = 1
 
@@ -199,7 +200,7 @@ def char_split(raw_img_input):
         对于白底黑字情况，则反之'''
         for m in range(start_row + 1, image_height + 1):
             if (row_black_num[m] if arg else row_white_num[m]) > \
-                    (0.95 * row_black_num_max if arg else 0.95 * row_white_num_max):  # 0.95这个参数请多调整，对应下面的0.05
+                    (0.9 * row_black_num_max if arg else 0.9 * row_white_num_max):  # 0.95这个参数请多调整，对应下面的0.05
                 end_row = m
                 break
         return end_row
@@ -231,10 +232,10 @@ def char_split(raw_img_input):
         row_white_num.append(white_num)    # 记录该行的白色像素总数
         row_black_num.append(black_num)    # 记录该行的黑色像素总数
 
-    arg = False
+    arg = True
     '''arg = True表示黑底白字, arg = False表示白底黑字'''
-    if row_black_num_max > row_white_num_max:
-        arg = True
+    if row_black_num_max < row_white_num_max:
+        arg = False
 
     row_mark = 1
 
@@ -244,7 +245,7 @@ def char_split(raw_img_input):
         即将该行看做出现字符的起始行。
         对于白底黑字情况，则反之'''
         if (row_white_num[row_mark] if arg else row_black_num[row_mark]) > \
-                (0.05 * row_white_num_max if arg else 0.05 * row_black_num_max):
+                (0.1 * row_white_num_max if arg else 0.1 * row_black_num_max):
 
             row_find_end = find_end_row(row_mark)
 
@@ -293,18 +294,26 @@ def char_split(raw_img_input):
                     for m in range(start_column, char_width + 1):
                         # 0.95这个参数请多调整，对应下面的0.05
                         if (column_black_num[m] if char_arg else column_white_num[m]) > \
-                                (0.95 * column_black_num_max if arg else 0.95 * column_white_num_max):
+                                (0.96 * column_black_num_max if arg else 0.96 * column_white_num_max):
 
                             end_column = m
+
                             break
+
                     return end_column
 
                 # 首先，依据亮度值得到黑白图像（虽然像素值只有0与255，但有BGR三个维度）
                 # char_gen_raw_img = gen_thresh_img(binary_img, threshold=65, mode='fixed')  # 生成gen_raw_img图片格式为PIL格式
                 char_gen_img = cv2.cvtColor(np.asarray(binary_img), cv2.COLOR_RGB2BGR)  # 转换为OpenCV图片格式
+                cv2.imshow('char_gen_img', char_gen_img)
+                cv2.waitKey(0)
                 print('Complete the format of char_split method from Image to OpenCV')
                 char_img_gray = cv2.cvtColor(char_gen_img, cv2.COLOR_BGR2GRAY)
                 _, char_thresh_img = cv2.threshold(char_img_gray, 127, 255, cv2.THRESH_BINARY)  # 得到二值图
+
+                cv2.imshow('char_thresh_img', char_thresh_img)
+                cv2.waitKey(0)
+
                 # thresh_img = cv2.cvtColor(np.asarray(thresh_img), cv2.COLOR_RGB2BGR)
                 column_white_num = []  # 记录每一列的白色像素总和
                 column_black_num = []  # ..........黑色.......
@@ -313,6 +322,7 @@ def char_split(raw_img_input):
                 char_width = char_thresh_img.shape[1]
                 column_white_num_max = 0
                 column_black_num_max = 0
+
                 # 计算每一列的黑白色像素总和
                 for i in range(char_width):
                     white_num = 0  # 这一列白色总数
@@ -322,15 +332,22 @@ def char_split(raw_img_input):
                             white_num += 1
                         if char_thresh_img[j][i] == 0:
                             black_num += 1
-                    column_white_num_max = max(column_white_num_max, white_num)
-                    column_black_num_max = max(column_black_num_max, black_num)
+
+                    temp_column_white_num_max = max(column_white_num_max, white_num)
+                    temp_column_black_num_max = max(column_black_num_max, black_num)
+
+                    # 排除列中出现的框线引起column_white_num_max=char_image_height情况
+                    if char_image_height - temp_column_white_num_max > 1:
+                        column_white_num_max = temp_column_white_num_max
+                        column_black_num_max = temp_column_black_num_max
+
                     column_white_num.append(white_num)  # 记录该列的白色像素总数
                     column_black_num.append(black_num)  # 记录该列的黑色像素总数
 
-                char_arg = False
+                char_arg = True
                 '''char_arg = True表示黑底白字, char_arg = False表示白底黑字'''
-                if column_black_num_max > column_white_num_max:
-                    char_arg = True
+                if column_black_num_max < column_white_num_max:
+                    char_arg = False
 
                 column_mark = 1
 
@@ -338,14 +355,113 @@ def char_split(raw_img_input):
                     column_mark += 1
                     '''char_arg = True，即黑底白字情况下，第n列的白色像素数目和 > 0.05 * （所有列中白色像素数目和的最大值），
                     即将该列看做出现字符的起始列。
-                    对于白底黑字情况，则反之'''
-                    if (column_white_num[column_mark] if char_arg else column_black_num[column_mark]) > \
+                    对于白底黑字情况，则反之；
+                    注：该种情况会将小数点过滤掉，因此此处需添加对小数点的支持，
+                    设定规则为当检测到该列字符像素点小于0.05*最大白色像素列，则若其列宽也<=2，则将其判定为小数点，
+                    若不是这种情况，则直接将一列长结束位置重新复制为检测列起始位置，开始下一轮字符检测；
+                    '''
+                    # 首先检测列方框线，判定规则为列框线长度与行高之差在1之内，若检测到，则直接过滤进入下轮检测。
+                    if (column_white_num_max if char_arg else column_black_num_max) - \
+                            (column_white_num[column_mark] if char_arg else column_black_num[column_mark]) <= 1:
+
+                        continue
+
+                    elif (column_white_num[column_mark] if char_arg else column_black_num[column_mark]) > \
                             (0.05 * column_white_num_max if char_arg else 0.05 * column_black_num_max):
 
                         column_find_end = find_end_column(column_mark)
 
-                        # 最短字符长度为3，因此要求字符列长必须在3以上（后续优化时候还要求宽高比来验证是否出现粘连）
-                        if column_find_end - column_mark > 2:  # 要求切割字符行需要有2列以上的距离也是为了保证排除直线的干扰
+                        # 对于一般字符判定：最短字符长度为3，因此要求字符列长必须在3以上
+                        # 对于数字1判定：当列包含字符长度高于一定阈值，则即便列宽为1依旧视为字符处理
+                        if column_find_end - column_mark <= 2:
+                            # 首先进行数字1判定，拿开始列字符所含字符像素数目大于所有列中最大字符像素数的0.8作为判定条件
+                            if column_find_end - column_mark == 1:  # 列宽为1
+                                if column_white_num[column_mark] / column_white_num_max >= 0.5:
+
+                                    column_split_start = column_mark - 1 \
+                                        if column_mark > 1 else column_mark
+
+                                    column_split_end = column_find_end + 1 \
+                                        if column_find_end + 1 < char_width else column_find_end
+
+                                    # 对应上述标准完成原图切割、显示、保存（注意是对原图的切割，而非二值图）
+                                    raw_image_split_char = raw_image_split_row[
+                                                           0:char_image_height,
+                                                           column_split_start:column_split_end
+                                                           ]
+
+                                    cv2.imshow('raw_image_split_char', raw_image_split_char)
+                                    cv2.waitKey(0)
+                                    cv2.destroyAllWindows()
+                                    cv2.imwrite('../Img_processed/char_split_{}.jpg'.format(column_mark - 1),
+                                                raw_image_split_char, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+                                # 进行小数点判定，阈值设到0.34保证能承受大约3个像素点
+                                elif column_white_num[column_mark] / column_white_num_max < 0.34:
+
+                                    column_split_start = column_mark - 1 \
+                                        if column_mark > 1 else column_mark
+
+                                    column_split_end = column_find_end + 1 \
+                                        if column_find_end + 1 < char_width else column_find_end
+
+                                    # 对应上述标准完成原图切割、显示、保存（注意是对原图的切割，而非二值图）
+                                    raw_image_split_char = raw_image_split_row[
+                                                           0:char_image_height,
+                                                           column_split_start:column_split_end
+                                                           ]
+
+                                    cv2.imshow('raw_image_split_char', raw_image_split_char)
+                                    cv2.waitKey(0)
+                                    cv2.destroyAllWindows()
+                                    cv2.imwrite('../Img_processed/char_split_{}.jpg'.format(column_mark - 1),
+                                                raw_image_split_char, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+                            # 首先进行数字1判定，拿开始列字符所含字符像素数目大于所有列中最大字符像素数的0.8作为判定条件
+                            elif column_find_end - column_mark == 2:  # 列宽为1
+                                if column_white_num[column_mark] / column_white_num_max >= 0.5 or \
+                                        column_white_num[column_mark+1] / column_white_num_max >= 0.5:
+
+                                    column_split_start = column_mark - 1 \
+                                        if column_mark > 1 else column_mark
+
+                                    column_split_end = column_find_end + 1 \
+                                        if column_find_end + 1 < char_width else column_find_end
+
+                                    # 对应上述标准完成原图切割、显示、保存（注意是对原图的切割，而非二值图）
+                                    raw_image_split_char = raw_image_split_row[
+                                                           0:char_image_height,
+                                                           column_split_start:column_split_end
+                                                           ]
+
+                                    cv2.imshow('raw_image_split_char', raw_image_split_char)
+                                    cv2.waitKey(0)
+                                    cv2.destroyAllWindows()
+                                    cv2.imwrite('../Img_processed/char_split_{}.jpg'.format(column_mark - 1),
+                                                raw_image_split_char, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+                                # 进行小数点判定，同理，阈值设到0.34，保证能承受大约3个像素值
+                                elif column_white_num[column_mark] / column_white_num_max and \
+                                        column_white_num[column_mark+1] / column_white_num_max < 0.34:
+
+                                    column_split_start = column_mark - 1 \
+                                        if column_mark > 1 else column_mark
+
+                                    column_split_end = column_find_end + 1 \
+                                        if column_find_end + 1 < char_width else column_find_end
+
+                                    # 对应上述标准完成原图切割、显示、保存（注意是对原图的切割，而非二值图）
+                                    raw_image_split_char = raw_image_split_row[
+                                                           0:char_image_height,
+                                                           column_split_start:column_split_end
+                                                           ]
+
+                                    cv2.imshow('raw_image_split_char', raw_image_split_char)
+                                    cv2.waitKey(0)
+                                    cv2.destroyAllWindows()
+                                    cv2.imwrite('../Img_processed/char_split_{}.jpg'.format(column_mark - 1),
+                                                raw_image_split_char, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+                        elif column_find_end - column_mark > 2:  # 要求切割字符行需要有2列以上的距离也是为了保证排除直线的干扰
 
                             '''此处开始判定是否出现黏连字符情况，此处只考虑双字符黏连情况'''
                             char_split_width = column_find_end - column_mark
@@ -466,7 +582,37 @@ def char_split(raw_img_input):
 
                         column_mark = column_find_end  # 不管检测到的列是不是字符，都将结束列重新赋值给开始列，进行下一轮循环
 
-            row_mark = row_split_end  # 无论是否进行行切割，将本次切割结束位置复制给下一次行切割初始点
+                    elif 0 < (column_white_num[column_mark] if char_arg else column_black_num[column_mark]) <= \
+                            (0.05 * column_white_num_max if char_arg else 0.05 * column_black_num_max):
+
+                        column_find_end = find_end_column(column_mark)
+                        # 添加对小数点起始检测列检测
+                        # 对于一般字符判定：最短字符长度为3，因此要求字符列长必须在3以上
+                        # 对于数字1判定：当列包含字符长度高于一定阈值，则即便列宽为1依旧视为字符处理
+                        # 进行小数点判定，拿开始列字符所含字符像素数目小于所有列中最大字符像素数的0.2作为判定条件
+                        if column_find_end - column_mark <= 2:
+
+                            if column_white_num[column_mark] / column_white_num_max <= 0.2:
+
+                                column_split_start = column_mark - 1 \
+                                    if column_mark > 1 else column_mark
+
+                                column_split_end = column_find_end + 1 \
+                                    if column_find_end + 1 < char_width else column_find_end
+
+                                # 对应上述标准完成原图切割、显示、保存（注意是对原图的切割，而非二值图）
+                                raw_image_split_char = raw_image_split_row[
+                                                       0:char_image_height,
+                                                       column_split_start:column_split_end
+                                                       ]
+
+                                cv2.imshow('raw_image_split_char', raw_image_split_char)
+                                cv2.waitKey(0)
+                                cv2.destroyAllWindows()
+                                cv2.imwrite('../Img_processed/char_split_{}.jpg'.format(column_mark - 1),
+                                            raw_image_split_char, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            # row_find_end
+            row_mark = row_find_end  # 无论是否进行行切割，将本次切割结束位置复制给下一次行切割初始点
 
 
 def detect_row_img_color(image):
@@ -483,7 +629,6 @@ def detect_row_img_color(image):
     bgr_light_blue = [176, 181, 57]  # 淡蓝判定基准
     bgr_yellow = [98, 158, 195]  # 黄色判定基准
     bgr_white = [225, 225, 225]  # 白色判定基准
-    bgr_gray = [100, 100, 100]  # 灰色判定基准
 
     def detect_img_submodule(submodule_img, color_to_check):
 
@@ -534,10 +679,10 @@ def detect_row_img_color(image):
         total_white_num = sum(row_white_num)
         total_black_num = sum(row_black_num)
 
-        arg = False
+        arg = True
         '''arg = True表示黑底白字, arg = False表示白底黑字'''
-        if total_black_num > total_white_num:
-            arg = True
+        if total_black_num < total_white_num:
+            arg = False
         # if (row_white_num[row_mark] if arg else row_black_num[row_mark])
         # 因为前面使用的是二值化方法是cv2.THRESH_BINARY，二值图表现为白底黑字
         if (total_white_num if arg else total_black_num) > (total_black_num * 0.01 if arg else total_white_num):
@@ -551,7 +696,31 @@ def detect_row_img_color(image):
     if check_color(detect_img_submodule(img, bgr_red)):
         print('This row image is red')
         # 依照红色字体对应亮度进行二值化处理
-        final_binary_img = gen_thresh_img(img, threshold=65, mode='dynamic')
+        # 因为传入的是切割过后的行图像
+        # 为了区分红色与灰色，不能再直接传原图，而是传转换到LAB空间处理过的图
+        cv2.imshow("the raw red img input", img)
+        cv2.waitKey(0)
+
+        red_img_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+        # convert 1D array to 3D, then convert it to LAB and take the first element
+
+        # python
+        bgr_red = [40, 40, 200]
+        bgr = bgr_red
+        thresh = 40
+
+        lab = cv2.cvtColor(np.uint8([[bgr]]), cv2.COLOR_BGR2LAB)[0][0]
+
+        min_lab = np.array([lab[0] - thresh, lab[1] - thresh, lab[2] - thresh])
+        max_lab = np.array([lab[0] + thresh, lab[1] + thresh, lab[2] + thresh])
+
+        mask_lab = cv2.inRange(red_img_lab, min_lab, max_lab)
+        filter_for_red_img = cv2.bitwise_and(red_img_lab, red_img_lab, mask=mask_lab)
+
+        cv2.imshow("the changed red img input", filter_for_red_img)
+        cv2.waitKey(0)
+        final_binary_img = gen_thresh_img(filter_for_red_img, threshold=65, mode='dynamic')
 
     elif check_color(detect_img_submodule(img, bgr_light_blue)):
         print('This row image is light blue')
@@ -566,19 +735,19 @@ def detect_row_img_color(image):
     elif check_color(detect_img_submodule(img, bgr_white)):
         print('This row image is light white')
         # 依照白色字体对应亮度进行二值化处理
-        final_binary_img = gen_thresh_img(img, threshold=180, mode='dynamic')
+        final_binary_img = gen_thresh_img(img, threshold=160, mode='dynamic')
 
     else:
         print('The color of this row image out of index')
         # 对其他情况，对应亮度进行二值化处理，取一个尽量低的值保证所有字体显现
-        final_binary_img = gen_thresh_img(img, threshold=65, mode='dynamic')
+        final_binary_img = gen_thresh_img(img, threshold=130, mode='dynamic')
 
     return final_binary_img
 
 
 if __name__ == '__main__':
     # input_image = "../Img_processed/row_split_18.jpg"
-    input_image = "../Image/2.jpg"
+    input_image = "../Image/3.jpg"
     # print(get_dominant_color(Image.open("../Image/2.JPG")))
     # get_dominant_color(input_image)
 
